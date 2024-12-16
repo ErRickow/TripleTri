64a2a0045931bbbcaa72e4d748bf0cf62df82a3d
Just R <remixingmagelang@gmail.com>
2024-12-16 18:23:29 +0700

    update Tic/tictac.py

--- Tic/tictac.py
+++ Tic/tictac.py
@@ -2,7 +2,6 @@ import json
 from Tic.emoji import *
 from pyrogram.types import InlineKeyboardButton
 
-
 class ErGame:
     def __init__(self, game_id: str, player1: dict, player2: dict = None) -> None:
         self.game_id = game_id
@@ -11,195 +10,70 @@ class ErGame:
         self.winner = None
         self.winner_keys = []
         self.whose_turn = True  # True: Player1, False: Player2
-        self.board = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
-        self.board_keys = [
-            [InlineKeyboardButton(
-                ".",
-                json.dumps({
-                    "type": "K",
-                    "coord": (i, j),
-                    "end": False
-                })
-            ) for j in range(3)]
-            for i in range(3)
-        ]
+        self.board = [[0] * 3 for _ in range(3)]
+        self.board_keys = [[InlineKeyboardButton(".", callback_data=f"K:{i}:{j}") for j in range(3)] for i in range(3)]
 
     def is_draw(self) -> bool:
-        for i in range(3):
-            for j in range(3):
-                if not self.board[i][j]:
-                    return False
+        if any(0 in row for row in self.board):
+            return False
 
-        new_board_keys = []
-
-        for i in range(3):
-
-            temp = []
-
-            for j in range(3):
-                if self.board[i][j] == 0:
-                    temp.append(
-                        InlineKeyboardButton(
-                            ".",
-                            json.dumps({
-                                "type": "K",
-                                "coord": (i, j),
-                                "end": True
-                            })
-                        )
-                    )
-                elif self.board[i][j] == 1:
-                    temp.append(
-                        InlineKeyboardButton(
-                            X_loser,
-                            json.dumps({
-                                "type": "K",
-                                "coord": (i, j),
-                                "end": True
-                            })
-                        )
-                    )
-                else:
-                    temp.append(
-                        InlineKeyboardButton(
-                            O_loser,
-                            json.dumps({
-                                "type": "K",
-                                "coord": (i, j),
-                                "end": True
-                            })
-                        )
-                    )
-
-            new_board_keys.append(temp)
-
-        new_board_keys.append(
-            [InlineKeyboardButton(
-                "Main Lagi!",
-                json.dumps({
-                    "type": "R"
-                })
-            )]
-        )
-
-        self.board_keys = new_board_keys
-
+        # Update all buttons to final state
+        self.update_buttons(final=True)
         return True
 
     def fill_board(self, player_id: int, coord: tuple) -> bool:
-        if self.board[coord[0]][coord[1]]:
+        if self.board[coord[0]][coord[1]] != 0:
             return False
 
-        if player_id == self.player1["id"]:
-            self.board[coord[0]][coord[1]] = 1
-            self.board_keys[coord[0]][coord[1]] = InlineKeyboardButton(
-                X,
-                json.dumps({
-                    "type": "K",
-                    "coord": coord,
-                    "end": False
-                })
-            )
-        else:
-            self.board[coord[0]][coord[1]] = 2
-            self.board_keys[coord[0]][coord[1]] = InlineKeyboardButton(
-                O,
-                json.dumps({
-                    "type": "K",
-                    "coord": coord,
-                    "end": False
-                })
-            )
-
+        symbol = 1 if player_id == self.player1["id"] else 2
+        emoji = "X" if symbol == 1 else "O"
+        self.board[coord[0]][coord[1]] = symbol
+        self.board_keys[coord[0]][coord[1]] = InlineKeyboardButton(
+            emoji, callback_data=f"K:{coord[0]}:{coord[1]}:F"
+        )
         return True
 
     def check_winner(self) -> bool:
-        if self.board[0][0] == self.board[0][1] == self.board[0][2] != 0:
-            self.winner = self.player1 if self.board[0][0] == 1 else self.player2
-            self.winner_keys.extend([(0, 0), (0, 1), (0, 2)])
-        elif self.board[0][0] == self.board[1][0] == self.board[2][0] != 0:
-            self.winner = self.player1 if self.board[0][0] == 1 else self.player2
-            self.winner_keys.extend([(0, 0), (1, 0), (2, 0)])
-        elif self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
-            self.winner = self.player1 if self.board[0][0] == 1 else self.player2
-            self.winner_keys.extend([(0, 0), (1, 1), (2, 2)])
-        elif self.board[1][0] == self.board[1][1] == self.board[1][2] != 0:
-            self.winner = self.player1 if self.board[1][1] == 1 else self.player2
-            self.winner_keys.extend([(1, 0), (1, 1), (1, 2)])
-        elif self.board[0][1] == self.board[1][1] == self.board[2][1] != 0:
-            self.winner = self.player1 if self.board[1][1] == 1 else self.player2
-            self.winner_keys.extend([(0, 1), (1, 1), (2, 1)])
-        elif self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
-            self.winner = self.player1 if self.board[1][1] == 1 else self.player2
-            self.winner_keys.extend([(0, 2), (1, 1), (2, 0)])
-        elif self.board[2][0] == self.board[2][1] == self.board[2][2] != 0:
-            self.winner = self.player1 if self.board[2][2] == 1 else self.player2
-            self.winner_keys.extend([(2, 0), (2, 1), (2, 2)])
-        elif self.board[0][2] == self.board[1][2] == self.board[2][2] != 0:
-            self.winner = self.player1 if self.board[2][2] == 1 else self.player2
-            self.winner_keys.extend([(0, 2), (1, 2), (2, 2)])
+        # Define all possible winning conditions
+        lines = [
+            # Rows
+            [(0, 0), (0, 1), (0, 2)],
+            [(1, 0), (1, 1), (1, 2)],
+            [(2, 0), (2, 1), (2, 2)],
+            # Columns
+            [(0, 0), (1, 0), (2, 0)],
+            [(0, 1), (1, 1), (2, 1)],
+            [(0, 2), (1, 2), (2, 2)],
+            # Diagonals
+            [(0, 0), (1, 1), (2, 2)],
+            [(0, 2), (1, 1), (2, 0)],
+        ]
 
-        if self.winner:
-            new_board_keys = []
-
-            for i in range(3):
-
-                temp = []
-
-                for j in range(3):
-                    if self.board[i][j] == 0:
-                        temp.append(
-                            InlineKeyboardButton(
-                                ".",
-                                json.dumps({
-                                    "type": "K",
-                                    "coord": (i, j),
-                                    "end": True
-                                })
-                            )
-                        )
-                    elif self.board[i][j] == 1:
-                        temp.append(
-                            InlineKeyboardButton(
-                                X if self.player1["id"] == self.winner["id"] and (i, j) in self.winner_keys
-                                else X_loser,
-                                json.dumps({
-                                    "type": "K",
-                                    "coord": (i, j),
-                                    "end": True
-                                })
-                            )
-                        )
-                    else:
-                        temp.append(
-                            InlineKeyboardButton(
-                                O if self.player2["id"] == self.winner["id"] and (i, j) in self.winner_keys
-                                else O_loser,
-                                json.dumps({
-                                    "type": "K",
-                                    "coord": (i, j),
-                                    "end": True
-                                })
-                            )
-                        )
-
-                new_board_keys.append(temp)
-
-            new_board_keys.append(
-                [InlineKeyboardButton(
-                    "Play again!",
-                    json.dumps({
-                        "type": "R"
-                    })
-                )]
-            )
-
-            self.board_keys = new_board_keys
-
-            return True
+        for line in lines:
+            values = [self.board[x][y] for x, y in line]
+            if values[0] != 0 and values.count(values[0]) == 3:
+                self.winner = self.player1 if values[0] == 1 else self.player2
+                self.winner_keys = line
+                self.update_buttons(final=True)
+                return True
 
         return False
+
+    def update_buttons(self, final: bool = False):
+        for i in range(3):
+            for j in range(3):
+                symbol = self.board[i][j]
+                if symbol == 0:
+                    emoji = "." if not final else " "
+                elif symbol == 1:
+                    emoji = "X" if final and (i, j) in self.winner_keys else "X"
+                else:
+                    emoji = "O" if final and (i, j) in self.winner_keys else "O"
+                self.board_keys[i][j] = InlineKeyboardButton(
+                    emoji, callback_data=f"K:{i}:{j}:{'F' if final else 'P'}"
+                )
+
+        if final:
+            self.board_keys.append(
+                [InlineKeyboardButton("Play Again!", callback_data="R")]
+            )
