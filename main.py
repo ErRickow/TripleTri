9f99c60e2ddc4ad76a8bd3b06b915f1a6fedea33
Just R <remixingmagelang@gmail.com>
2024-12-16 17:59:57 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 
 from Tic.data import *
 from Tic.emoji import *
+from doten import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent, \
     InlineKeyboardMarkup, CallbackQuery, Message
 
+load_dotenv()
 
 app = Client("er",
              api_id=os.environ.get("API_ID"),
