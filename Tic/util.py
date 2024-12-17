9adce08a5789dca1cd7fd1fe4a67e10f8c4f60c0
Just R <remixingmagelang@gmail.com>
2024-12-17 08:53:04 +0700

    update Tic/util.py

--- Tic/util.py
+++ Tic/util.py
@@ -10,8 +10,9 @@ def load_stats():
 
 # Fungsi untuk menyimpan statistik pengguna
 def save_stats(stats):
-    with open("user_stats.json", "w") as file:
-        json.dump(stats, file, indent=4)
+    try:
+        with open("user_stats.json", "w") as file:
+            json.dump(stats, file, indent=4)
     except Exception as e:
         print(e)
 
