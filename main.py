6afe53df067bd269fb7bfc9788ad14fd10445677
Just R <remixingmagelang@gmail.com>
2024-12-18 22:02:49 +0700

    update main.py

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def inline_query_handler(_, query: InlineQuery):
         results=[InlineQueryResultArticle(
             title="Tic-Tac-Toe",
             input_message_content=InputTextMessageContent(
-                f"**{namanya}** menantang untuk bermain!"
+                f"**Menantang untuk bermain!**"
             ),
             description="Pencet Disini Untuk Menantang Temanmu!",
             thumb_url="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Tic_tac_toe.svg/1200px-Tic_tac_toe"
