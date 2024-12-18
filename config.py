a8ffc80e7f9fa3cc0169f55e980376b093a13f20
Just R <remixingmagelang@gmail.com>
2024-12-19 01:35:28 +0700

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 MUST_JOIN = ["Er_support_group", "Ctbb_Un"]
 LOGS_GROUP_ID = -1002423575637
 ownr = [1448273246]
-sudo = []
-if ownr not in sudo:
-    sudo.append(ownr)
+SUDOERS = []
+if ownr not in SUDOERS:
+    SUDOERS.append(ownr)
