a36e7bd84072b62e3d5fac6a45cbcde8c37ee8e3
Just R <remixingmagelang@gmail.com>
2024-12-19 04:11:40 +0700

    update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def start_handler(bot: Client, message: Message):
         dB.add_to_var(bot.me.id, "BROADCAST", message.from_user.id)
     bot.send_message(
         message.chat.id,
-        f"<blockquote>Hi **{message.from_user.first_name}**,</blockquote>\n"
+        f"<blockquote>Hi **{org.first_name}**,</blockquote>\n"
         "<blockquote>Untuk memulai, kamu bisa langsung menggunakan fitur ini tanpa perlu menambahkan {bot.me.mention} ke grup.</blockquote>"
         "<blockquote>Yang penting, grup yang kamu pilih **tidak dalam kondisi terbatas (restricted)** dan memungkinkan penggunaan fitur ini.</blockquote>"
         "<blockquote>Klik tombol **Bermain** dan pilih grup mana pun yang kamu inginkan untuk memulai.</blockquote>",
